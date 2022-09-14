f961d44b8a7ee9ba0cc9dee2b0ddbc65aee1cf8e
ll-fox <657609759@qq.com>
2022-09-15 00:50:09 +0800

    feat: 1.2

--- components/Intro.js
+++ components/Intro.js
@@ -7,20 +7,21 @@ const Intro = () => {
     <div className={style.container}>
       {/* <div className={`${style.circle} ${style.circle1}`} />
       <div className={`${style.circle} ${style.circle2}`} /> */}
-      <Circle top="-45vh" left="-45vh" />
+      <Circle top="0" left="45vh" />
       {/* <Circle backgroundColor="#01c686" right="-40vh" /> */}
       <div className={style.card}>
         <h1 className={style.title}>
           <span className={style.brandName}>金翁农业</span> 科技 有限公司
         </h1>
+        <div className={style.head}></div>
         <p className={style.desc}>
           坐落于西北，一家致力于打造农产品供应链的公司<br></br>
           出生于西北，一群致力于建设家乡的人
         </p>
-        <button className={style.button}>DISCOVER</button>
+        {/* <button className={style.button}>DISCOVER</button> */}
       </div>
       <div className={style.card}>
-        <Image src="/img/zao.png" layout="fill" objectFit="cover" alt="" />
+        {/* <Image src="/img/zao.png" layout="fill" objectFit="cover" alt="" /> */}
       </div>
     </div>
   )
