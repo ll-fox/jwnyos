22d6217805df58343ecc31b62f1c962c6f892503
ll-fox <657609759@qq.com>
2022-11-05 00:48:31 +0800

    feat: 首页优化

--- components/Intro.js
+++ components/Intro.js
@@ -5,21 +5,25 @@ import Circle from './Circle'
 const Intro = () => {
   return (
     <div className={style.container}>
-      {/* <div className={`${style.circle} ${style.circle1}`} />
-      <div className={`${style.circle} ${style.circle2}`} /> */}
-      <Circle top="0" left="45vh" />
-      {/* <Circle backgroundColor="#01c686" right="-40vh" /> */}
+      <div className={style.pic1} />
+      <div className={style.pic2} />
+      <div className={style.pic3} />
+      <div className={style.pic4} />
+      <div className={style.text}>
+        善为至宝， 一生用之不尽
+        <br />
+        心作良田， 百世耕之有余
+      </div>
+      {/* <Circle top="0" left="45vh" /> */}
       <div className={style.card}>
         <h1 className={style.title}>
           <span className={style.brandName}>金翁农业</span> 科技有限公司
         </h1>
         <div className={style.head}></div>
-        <p className={style.desc}>
-          善为至宝， 一生用之不尽；心作良田， 百世耕之有余<br></br>
-          企业宗旨：利而不害 为而不争
-        </p>
+
         {/* <button className={style.button}>DISCOVER</button> */}
       </div>
+      <div className={style.desc}>利而不害 为而不争</div>
       <div className={style.card}>
         {/* <Image src="/img/zao.png" layout="fill" objectFit="cover" alt="" /> */}
       </div>
