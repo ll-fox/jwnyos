3979f5dbebeca0bb64b43aa0ae4b730eb1383df2
ll-fox <657609759@qq.com>
2023-04-20 21:49:59 +0800

    feat：添加广告弹窗

--- components/AdsModal.jsx
+++ components/AdsModal.jsx
@@ -1,8 +1,6 @@
 import React, { useRef, useEffect, useMemo, useState } from 'react'
-import style from '../styles/AdsModal.module.css'
 import Link from 'next/link'
-
-
+import styles from '../styles/AdsModal.module.css'
 
 function AdsModal(props) {
   const { visible = false, title, children, footer } = props
@@ -18,10 +16,9 @@ function AdsModal(props) {
   return (
     <>
       {visible && (
-
-            <div ref={ref} className={style['H-Dialog-overview']}>
-            <div className={style['H-Dialog-inner']}>
-                <div className={style['dialog-header']}>
+            <div ref={ref} className={styles['H-Dialog-overview']}>
+            <div className={styles['H-Dialog-inner']}>
+                <div className={styles['dialog-header']}>
                 <h3>{title}</h3>
                 </div>
                 <Link
@@ -29,10 +26,10 @@ function AdsModal(props) {
                     href="https://shop308693260.taobao.com/index.htm?spm=a1z10.1-c.w5002-24669166804.2.3e7b68c4xSF0g8"
                     >
                     <a target="_blank">
-                    <div className={style['dialog-body']}>{children}</div>
+                    <div className={styles['dialog-body']}>{children}</div>
                     </a>          
                 </Link>
-                <div className={style['dialog-footer']}>
+                <div className={styles['dialog-footer']}>
                 {footer}
                 </div>
             </div>
